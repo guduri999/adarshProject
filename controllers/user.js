18f15f421dfec7f6a921fdf302ee953e9b8b3422
restaurent999 <gudurisandeep999@gmail.com>
2023-06-16 02:11:23 +0530

    login api completed

--- controllers/user.js
+++ controllers/user.js
@@ -2,11 +2,21 @@ import userModal from "../modals/index.js";
 
 export const userAuthentication = async (req, res) => {
 
-    const data = await userModal.findOne({ userName: req.body.userName, password: req.body.password })
+    const { username, password } = req.body.requestparameters
+    const data = await userModal.findOne({ mobile: username, password: password })
 
     res.status(200).json({
-        "response": "success",
-        data: (data) ? data : "credentials not valid"
+        "code": 200,
+        "message": "Login Successful",
+        "result": {
+            "user_id": data.user_id,
+            "name": data.name,
+            "role": data.role,
+            "mobile": data.mobile,
+            "username": data.username,
+            "status": data.status
+        },
+        "requestname": "login"
     })
 
 }
--- schema/user.schema.js
+++ schema/user.schema.js
@@ -1,11 +1,30 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    userName: {
+    // userName: {
+    //     type: String,
+    //     required: true
+    // },
+    // password: {
+    //     type: String,
+    //     required: true
+    // }
+    user_id: {
         type: String,
         required: true
-    },
-    password: {
+    }, name: {
+        type: String,
+        required: true
+    }, role: {
+        type: String,
+        required: true
+    }, mobile: {
+        type: String,
+        required: true
+    }, username: {
+        type: String,
+        required: true
+    }, status: {
         type: String,
         required: true
     }
