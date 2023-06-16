6dc79379b73bcf9afb5d2b5d169afceee39d0e39
restaurent999 <gudurisandeep999@gmail.com>
2023-06-16 17:41:27 +0530

    raw data functionality added

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,17 @@
 import userModal from "../modals/index.js";
+// import { raw } from "express";
+import raw from 'raw-body'
 
 export const userAuthentication = async (req, res) => {
 
-    const { username, password } = req.body.requestparameters
+    var body = (await raw(req));
+
+    body = body.toString();
+
+    body = JSON.parse(body);
+
+    const { username, password } = body.requestparameters
+
     const data = await userModal.findOne({ mobile: username, password: password })
 
     if (data) {
@@ -26,6 +35,26 @@ export const userAuthentication = async (req, res) => {
             "result": [],
             "requestname": "login"
         })
+    }
+
+}
+
+export const fetchCustomer = async (req, res) => {
+
+    var body = (await raw(req));
+
+    body = body.toString();
+
+    body = JSON.parse(body);
+
+    console.log(body.requestparameters)
+
+    const data = await userModal.findOne(body.requestparameters)
+
+    if (data) {
+
+    } else {
+        await res.status(200).json({ Response: body.requestparameters })
     }
 
 }
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { userAuthentication } from '../controllers/user.js';
+import { fetchCustomer, userAuthentication } from '../controllers/user.js';
 
 const userRoutes = express.Router();
 
 userRoutes.post('/authentication', userAuthentication)
+userRoutes.post('/fetchCustomer', fetchCustomer)
 
 export default userRoutes;
