4a0c4af10c154471764d1dcb10a5146da86709e3
restaurent999 <gudurisandeep999@gmail.com>
2023-06-16 14:42:03 +0530

    login api error objected added

--- controllers/user.js
+++ controllers/user.js
@@ -5,18 +5,27 @@ export const userAuthentication = async (req, res) => {
     const { username, password } = req.body.requestparameters
     const data = await userModal.findOne({ mobile: username, password: password })
 
-    res.status(200).json({
-        "code": 200,
-        "message": "Login Successful",
-        "result": {
-            "user_id": data.user_id,
-            "name": data.name,
-            "role": data.role,
-            "mobile": data.mobile,
-            "username": data.username,
-            "status": data.status
-        },
-        "requestname": "login"
-    })
+    if (data) {
+        res.status(200).json({
+            "code": 200,
+            "message": "Login Successful",
+            "result": {
+                "user_id": data.user_id,
+                "name": data.name,
+                "role": data.role,
+                "mobile": data.mobile,
+                "username": data.username,
+                "status": data.status
+            },
+            "requestname": "login"
+        })
+    } else {
+        res.status(400).json({
+            "code": "400",
+            "message": "Mismatch in Username or Password. Please try again with currect credentials",
+            "result": [],
+            "requestname": "login"
+        })
+    }
 
 }
